d023acd8afb42ffd9c969abc708196ccf88a1e2c
tommaso.dare <tommaso.dare96@gmail.com>
2025-04-15 15:11:27 +0200

    Add Show and Index Routes

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const connection = require('./data/db.js');
+
+// Middleware
+app.use(cors(
+    {
+        origin: process.env.FRONT_URL || 'http://localhost:5173',
+    }
+));
+
 
 // Middleware per parsing JSON
 app.use(express.json());
@@ -16,6 +25,8 @@ app.listen(PORT, () => {
     console.log(`Server attivo su http://localhost:${PORT}`);
 });
 
+
+// Rotta Index
 app.get('/api/v1/movies', (req, res) => {
 
     // Definiamo la query SQL per ottenere i film
@@ -25,4 +36,21 @@ app.get('/api/v1/movies', (req, res) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
         res.json(results);
     })
+})
+
+
+// Rotta Show
+app.get('/api/v1/movies/:id', (req, res) => {
+
+    // Recuperiamo l'id dall'URL
+    const id = req.params.id
+
+    const sql = 'SELECT * FROM movies WHERE id = ?'
+
+    // Eseguiamo la query
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+        res.json(results[0]);
+    });
 })
